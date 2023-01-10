cfddb3badf0c125861595f54d43be188af28ca80
Manzzeli <110908915+Manzzeli@users.noreply.github.com>
2023-01-10 11:42:43 +0200

    Add files via upload

--- shiping_manzili/models/models.py
+++ shiping_manzili/models/models.py
@@ -6,6 +6,7 @@ class shiping(models.Model):
     _inherit = 'sale.order.line'
 
     shipping = fields.Float(string="shiping",  required=False, )
+    # is_new_field = fields.Boolean(string="TF",  )
 
     @api.depends('shipping')
     def _compute_amount(self):
