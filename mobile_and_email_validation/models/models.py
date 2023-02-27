f498badd6eee48e81980475803a905a5bedf4a09
Manzzeli <110908915+Manzzeli@users.noreply.github.com>
2023-02-27 14:26:57 +0200

    Add files via upload

--- mobile_and_email_validation/models/models.py
+++ mobile_and_email_validation/models/models.py
@@ -1,18 +1,43 @@
-from odoo import models
+from odoo import models, _, api, fields
+import re
+from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError
 
 
 class contactsemailandphone(models.Model):
     _inherit = 'res.partner'
 
-    _sql_constraints = [
-        ('unique_mobile', 'unique (mobile)', 'The mobile must be unique!'),
-        ('unique_phone', 'unique (phone)', 'The phone must be unique!'),
-    ]
+    @api.onchange('phone')
+
+
+    def set_constrains_res_partner_phone(self):
+        if self.phone:
+            phone_number = self.env['crm.lead'].search([('phone', '=', self.phone)])
+            if phone_number:
+                raise ValidationError(_("There is another Contact with same Phone."))
+
+
+    @api.onchange('mobile')
+    def set_constrains_res_partner_mobile(self):
+        if self.mobile:
+            mobile_number = self.env['crm.lead'].search([('mobile', '=', self.mobile)])
+            if mobile_number:
+                raise ValidationError(_("There is another Contact with same mobile."))
 
 
 class contactsemailandphonecrm(models.Model):
     _inherit = 'crm.lead'
-    _sql_constraints = [
-        ('phone_uniq', 'unique(phone)', 'The phone must be unique!'),
-        ('mobile_uniq', 'unique(mobile)', 'The mobile must be unique!'),
-    ]
+
+    @api.onchange('phone')
+    def set_constrains_phone(self):
+        if self.phone:
+            phone_number = self.env['crm.lead'].search([('phone', '=', self.phone)])
+            if phone_number:
+                raise UserError(_("There is another Contact with same Phone."))
+
+    @api.onchange('mobile')
+    def set_constrains_mobile(self):
+        if self.mobile:
+            mobile_number = self.env['crm.lead'].search([('mobile', '=', self.mobile)])
+            if mobile_number:
+                raise UserError(_("There is another Contact with same mobile."))
