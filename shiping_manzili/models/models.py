1b1666d0b76af88dd55e6dc319b33ed224e05408
Manzzeli <110908915+Manzzeli@users.noreply.github.com>
2023-03-14 11:52:52 +0200

    Add files via upload

--- shiping_manzili/models/models.py
+++ shiping_manzili/models/models.py
@@ -1,10 +1,18 @@
-from odoo import api, fields, models
+from odoo import api, fields, models,_
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.float_utils import float_compare, float_is_zero, float_round
+from itertools import groupby
+
+
+
+
 
 
 class shiping(models.Model):
     _inherit = 'sale.order.line'
 
     shipping = fields.Float(string="Shiping",  required=False, )
+    # new_field_id = fields.Many2one(comodel_name="purchase.order.line", string="", required=False, )
     # price_total = fields.Monetary(compute='_compute_amount', string='Total', store=True)
     # price_subtotal = fields.Monetary(string='Subtotal', store=True, readonly=True,
     #                                  currency_field='currency_id')
@@ -37,12 +45,12 @@ class accountmovelineshiping(models.Model):
     _inherit = 'account.move.line'
 
     re_shiping_test = fields.Float(string="Shiping",  required=False,)
-
+    purchase_shipping = fields.Float(string="P Shiping",  required=False,)
     @api.model_create_multi
     def create(self, vals_list):
         # OVERRIDE
         ACCOUNTING_FIELDS = ('debit', 'credit', 'amount_currency')
-        BUSINESS_FIELDS = ('price_unit', 'quantity', 'discount', 'tax_ids','re_shiping_test')
+        BUSINESS_FIELDS = ('price_unit', 'quantity', 'discount', 'tax_ids','re_shiping_test','purchase_shipping')
 
         for vals in vals_list:
             move = self.env['account.move'].browse(vals['move_id'])
@@ -83,6 +91,7 @@ class accountmovelineshiping(models.Model):
                         taxes,
                         move.move_type,
                         vals.get('re_shiping_test', 0.0),
+                        vals.get('purchase_shipping', 0.0),
                     ).get('price_subtotal', 0.0)
                     vals.update(self._get_fields_onchange_balance_model(
                         vals.get('quantity', 0.0),
@@ -103,6 +112,7 @@ class accountmovelineshiping(models.Model):
                         taxes,
                         move.move_type,
                         vals.get('re_shiping_test', 0.0),
+                        vals.get('purchase_shipping', 0.0),
 
                     ))
                 elif any(vals.get(field) for field in BUSINESS_FIELDS):
@@ -116,6 +126,7 @@ class accountmovelineshiping(models.Model):
                         taxes,
                         move.move_type,
                         vals.get('re_shiping_test', 0.0),
+                        vals.get('purchase_shipping', 0.0),
 
                     ))
                     vals.update(self._get_fields_onchange_subtotal_model(
@@ -138,7 +149,7 @@ class accountmovelineshiping(models.Model):
         return lines
 
     def _get_price_total_and_subtotal(self, price_unit=None, quantity=None, discount=None, currency=None, product=None,
-                                      partner=None, taxes=None, move_type=None,re_shiping_test=None,):
+                                      partner=None, taxes=None, move_type=None,re_shiping_test=None,purchase_shipping=None):
         self.ensure_one()
         return self._get_price_total_and_subtotal_model(
             price_unit=price_unit or self.price_unit,
@@ -150,11 +161,12 @@ class accountmovelineshiping(models.Model):
             taxes=taxes or self.tax_ids,
             move_type=move_type or self.move_id.move_type,
             re_shiping_test=re_shiping_test or self.re_shiping_test,
+            purchase_shipping=purchase_shipping or self.purchase_shipping,
         )
 
     @api.model
     def _get_price_total_and_subtotal_model(self, price_unit, quantity, discount, currency, product, partner, taxes,
-                                            move_type,re_shiping_test=0):
+                                            move_type,re_shiping_test=0,purchase_shipping=0):
         ''' this method is used to compute 'price_total' & 'price_subtotal'.
 
         :param price_unit:  the current price unit.
@@ -175,7 +187,7 @@ class accountmovelineshiping(models.Model):
 
         print(type(re_shiping_test))
         print(re_shiping_test)
-        subtotal = quantity * line_discount_price_unit + re_shiping_test
+        subtotal = quantity * line_discount_price_unit + re_shiping_test + purchase_shipping
 
         # compute 'price_total'.
         if taxes:
@@ -186,7 +198,7 @@ class accountmovelineshiping(models.Model):
                                                                                       product=product, partner=partner,
                                                                                       is_refund=move_type in (
                                                                                       'out_refund', 'in_refund'))
-            res['price_subtotal'] = taxes_res['total_excluded']+re_shiping_test
+            res['price_subtotal'] = taxes_res['total_excluded']+re_shiping_test+purchase_shipping
             res['price_total'] = taxes_res['total_included']
         else:
             res['price_total'] = res['price_subtotal'] = subtotal
@@ -195,13 +207,21 @@ class accountmovelineshiping(models.Model):
             res = {k: currency.round(v) for k, v in res.items()}
         return res
 
-    @api.onchange('quantity', 'discount', 'price_unit', 'tax_ids','re_shiping_test')
+    @api.onchange('quantity', 'discount', 'price_unit', 'tax_ids','re_shiping_test','purchase_shipping')
     def _onchange_price_subtotal(self):
         res = super(accountmovelineshiping, self)._onchange_price_subtotal()
         for rec in self:
             if rec.price_subtotal:
-                rec.price_subtotal = rec.re_shiping_test + rec.price_subtotal
+                rec.price_subtotal = rec.re_shiping_test +rec.purchase_shipping +rec.price_subtotal
         return res
+
+    # @api.depends('purchase_shipping')
+    # def _compute_amount(self):
+    #     res = super(accountmovelineshiping, self)._compute_amount()
+    #     for rec in self:
+    #         if rec.price_subtotal:
+    #             rec.price_subtotal = rec.purchase_shipping + rec.price_subtotal
+    #     return res
 
 # class accountmoveone(models.Model):
 #     _inherit = 'account.move'
@@ -256,3 +276,89 @@ class accountmovelineshiping(models.Model):
     #             rec.sum_amount=0
 
 
+
+
+class shipingpurchase(models.Model):
+    _inherit = 'purchase.order.line'
+
+    shipping_uu = fields.Float(string="Shiping",  required=False,related='sale_line_id.shipping')
+    # new_field_id = fields.Many2one(comodel_name="sale.order.line", string="Sale Order Line", required=False,)
+
+    @api.depends('shipping_uu')
+    def _compute_amount(self):
+        res = super(shipingpurchase, self)._compute_amount()
+        for rec in self:
+            if rec.price_subtotal:
+                rec.price_subtotal = rec.shipping_uu + rec.price_subtotal
+        return res
+
+    # def _prepare_invoice(self):
+    #     res = super(shiping, self)._prepare_invoice()
+    #     res.update({'purchase_shipping': self.shipping_uu,
+    #
+    #                 })
+    #     return res
+
+    def _prepare_account_move_line(self, move=False):
+        self.ensure_one()
+        aml_currency = move and move.currency_id or self.currency_id
+        date = move and move.date or fields.Date.today()
+        res = {
+            'display_type': self.display_type,
+            'sequence': self.sequence,
+            'name': '%s: %s' % (self.order_id.name, self.name),
+            'product_id': self.product_id.id,
+            'product_uom_id': self.product_uom.id,
+            'quantity': self.qty_to_invoice,
+            'price_unit': self.currency_id._convert(self.price_unit, aml_currency, self.company_id, date, round=False),
+            'tax_ids': [(6, 0, self.taxes_id.ids)],
+            'analytic_account_id': self.account_analytic_id.id,
+            'analytic_tag_ids': [(6, 0, self.analytic_tag_ids.ids)],
+            'purchase_line_id': self.id,
+            'purchase_shipping': self.shipping_uu,
+        }
+        if not move:
+            return res
+
+        if self.currency_id == move.company_id.currency_id:
+            currency = False
+        else:
+            currency = move.currency_id
+
+        res.update({
+            'move_id': move.id,
+            'currency_id': currency and currency.id or False,
+            'date_maturity': move.invoice_date_due,
+            'partner_id': move.partner_id.id,
+        })
+        return res
+
+
+# class NewModule(models.Model):
+#     _inherit = 'sale.order'
+#
+#     def action_view_purchase_orders(self):
+#         self.ensure_one()
+#         purchase_order_ids = self._get_purchase_orders().ids
+#         action = {
+#             'res_model': 'purchase.order',
+#             'type': 'ir.actions.act_window',
+#
+#         }
+#         if len(purchase_order_ids) == 1:
+#             action.update({
+#                 'view_mode': 'form',
+#                 'res_id': purchase_order_ids[0],
+#             })
+#         else:
+#             action.update({
+#                 'name': _("Purchase Order generated from %s", self.name),
+#                 'domain': [('id', 'in', purchase_order_ids)],
+#                 'view_mode': 'tree,form',
+#             })
+#         return action
+#
+#         # def _get_purchase_orders(self):
+#         #     return self.order_line.purchase_line_ids.shipping
+#         #
+#
