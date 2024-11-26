35eccc9cf949b007b5f2dd2d6dd74c02f324bf79
Aelassal <Aelassal>
2024-11-26 21:24:26 +0200

    new update

--- sale_report/__manifest__.py
+++ sale_report/__manifest__.py
@@ -16,7 +16,7 @@ Sale Reports custom.
         'report/sale_report_templates.xml',
         'report/account_move_report.xml',
         'report/po_report.xml',
-        'report/rfq_report.xml',
+        # 'report/rfq_report.xml',
     ],
 
     'installable': True,
