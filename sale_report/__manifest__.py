8b39bdfcbce7bd29a44701a360a2c7be6d25cbdc
Aelassal <Aelassal>
2024-11-26 21:32:38 +0200

    update report

---
+++ sale_report/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Sales Report',
+    'version': '1.2',
+    'category': 'Sales/Sales',
+    'summary': 'Sales internal machinery',
+    'description': """
+Sale Reports custom.
+    """,
+    'depends': ['sale_management', 'account', 'purchase'],
+    'data': [
+        # 'report/sale_report.xml',
+        'report/sale_report_templates.xml',
+        'report/account_move_report.xml',
+        'report/po_report.xml',
+        # 'report/rfq_report.xml',
+    ],
+
+    'installable': True,
+    'auto_install': False,
+
+}
